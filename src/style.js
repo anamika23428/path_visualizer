d3d33ce3e372761daca5e47fb21765564874c15d
anamika123 <anamika23428@gmail.com>
2024-12-29 12:39:22 +0530

    day1

---
+++ src/style.js
@@ -0,0 +1,7 @@
+{/* <script>
+    document.getElementById('path-options').addEventListener('change', function() {
+        const selectedOption = this.value;
+        console.log("Selected option:", selectedOption);
+        // Handle the selected option here (e.g., change the path or visual)
+    });
+</script> */}
